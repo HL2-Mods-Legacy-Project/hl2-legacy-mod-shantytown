f9f325f1ebba599db7cab6fcb1e837c917a53d09
Marc-Antoine Lortie <malortie@users.noreply.github.com>
2025-07-09 20:04:17 -0400

    Update build_all.ps1

--- scripts/build_all.ps1
+++ scripts/build_all.ps1
@@ -1,12 +1,10 @@
 & "$PSScriptRoot\..\gitmodules\mod-installer\build_all.ps1" `
-  -Version "<version>" `
+  -Version "2.0.0" `
   -PreReleaseVersion "beta.1" `
-  -OutputFilePrefix "<output file prefix>" `
-  -ModName "<mod name>" `
-  -ModFolder "<mod folder>" `
-  -AdditionalModFolders @() `
+  -OutputFilePrefix "Shantytown" `
+  -ModName "Shantytown" `
+  -ModFolder "Shantytown" `
   -ModFilesPath "$PSScriptRoot\..\game" `
-  -ModBaseFilesUrl "<url to mod base files zip>" `
-  -ModBaseFilesUrlHash "<mod base files zip sha256>" `
-  -ModReadmePath "<mod readme path>" `
+  -ModBaseFilesUrl "https://github.com/HL2-Mods-Legacy-Project/hl2-legacy-registry/releases/download/shanty-town/Shantytown-Original.zip" `
+  -ModBaseFilesUrlHash "0ca8c5e5b6f67d12ffe2b56e0c47f2acdfb2bb6cd6b2b85b69db43815608c5ae" `
   -PublishPath "$PSScriptRoot\..\artifacts\publish"
